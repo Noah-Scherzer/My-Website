4461ad474e62896d0fe331333326754fb448a1ae
Noah Scherzer <62837718+Noah-Scherzer@users.noreply.github.com>
2025-03-26 03:59:33 +0100

    update portfolio/background.js

--- portfolio/background.js
+++ portfolio/background.js
@@ -3,6 +3,7 @@
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
   const renderer = new THREE.WebGLRenderer({ antialias: true, canvas: canvas });
+  renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
 
   // Offscreen-Canvas für das Notizblock-Muster
